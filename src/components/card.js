9076cad83084fd2d437543cc90aec77d0fa449a1
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-12-26 13:26:37 +0100

    Create Board and Card Component

---
+++ src/components/card.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './card.css';
+
+export default function Card() {
+  const text = 'Example card';
+  return (
+    <div className="card">
+      {text}
+    </div>
+  );
+};
