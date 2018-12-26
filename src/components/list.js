72de9eb364491284b0e62e5d6b6141bec663a76e
Ivan Špoljarić <ivanspoljaric22@gmail.com>
2018-12-26 13:30:33 +0100

    Create List component

---
+++ src/components/list.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from './card';
+
+export default function List() {
+  return (
+    <div>
+      <h3>Example List</h3>
+      <ul className="list">
+        <li>
+          <Card />
+        </li>
+        <li>
+          <Card />
+        </li>
+        <li>
+          <Card />
+        </li>
+      </ul>
+    </div>
+  );
+};
